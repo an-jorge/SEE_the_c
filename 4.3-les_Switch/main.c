b1087881574be3f79448026e0701391feedc6faa
An Jorge <an.jorge@icloud.com>
2024-04-22 21:29:52 +0100

    Lesson 4.3

---
+++ 4.3-les_Switch/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int day;
+    printf("Insira o dia da Semana entre 1 a 7\n");
+    scanf_s("%d", &day);
+    switch (day)
+    {
+    case 1:
+        printf("Segunda-feira");
+        break;
+    case 2:
+        printf("Terca-feira");
+        break;
+    case 3:
+        printf("Quarta-feira");
+        break;
+    case 4:
+        printf("Quinta-feira");
+        break;
+    case 5:
+        printf("Sexta-feira");
+        break;
+    case 6:
+        printf("Sabado");
+        break;
+    case 7:
+        printf("Domingo");
+        break;
+    default:
+        break;
+    }
+}
