36e7cc74196e22a7023cbe94ac88b622651e89fd
An Jorge <an.jorge@icloud.com>
2024-05-22 22:46:40 +0100

    Done! Ex.01_Soma

---
+++ Exercicios/01/main.c
@@ -0,0 +1,18 @@
+/*
+
+Desenvolva um algoritmo em c para somar dois valores inteiros (10 + 5)
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    int number_a = 10;
+    int number_b = 5;
+    int sum;
+
+    sum = number_a + number_b;
+    printf("O resultado da soma entre %d + %d = %d", number_a, number_b, sum);
+
+    return 0;
+}
