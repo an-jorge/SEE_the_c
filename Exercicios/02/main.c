780d9ab8a3b6a0bb9c598b5243b1ef86734a7ec3
An Jorge <an.jorge@icloud.com>
2024-05-26 02:04:24 +0100

    Ex.02

---
+++ Exercicios/02/main.c
@@ -0,0 +1,18 @@
+/*
+Escreva um algoritmo que leia três notas e calcule e imprima a média
+aritmética.
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    int value1 = 10;
+    int value2 = 5;
+    int value3 = 8;
+    int sum;
+    printf("Notas do aluno \n[1]. %d\n[2]. %d\n[3]. %d\n", value1, value2, value3);
+    sum = value1 + value2 + value3 / 3;
+    printf("A nota media do aluno e %d valores",sum);
+    return 0;
+}
