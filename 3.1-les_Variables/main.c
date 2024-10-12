09212638e91f04ffcddce741038c6a128e35a545
An Jorge <an.jorge@icloud.com>
2024-10-12 21:53:26 +0100

    add _WIN32

--- 3.1-les_Variables/main.c
+++ 3.1-les_Variables/main.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 
-#define text "Entrada e saida de dados" // Variavel do tipo constante
+#define TEXT "Entrada e saida de dados" // Variavel do tipo constante
 
 int main()
 {
-    printf("%s \n", text);
+    printf("%s \n", TEXT);
     int age = 0;
     float height = 0.0;
     char name[50] = "";
-    
-    printf("Digite a idade: \n");
-    scanf_s("%d", &age);
-    printf("Digite a altura: \n");
-    scanf_s("%f", &height);
-    printf("Digite o nome: \n");
-    scanf_s("%s", &name);
 
+#ifdef _WIN32
+    printf("Digite a sua idade: \n");
+    scanf_s("%d", &age);
+    fflush(stdin);
+    printf("Digite a sua altura: \n");
+    scanf_s("%f", &height);
+    fflush(stdin);
+    printf("Digite o seu nome: \n");
+    scanf_s("%f", &name);
+    fflush(stdin);
+#else
+    printf("Digite a sua idade: \n");
+    scanf("%d", &age);
+    fflush(stdin);
+    printf("Digite sua a altura: \n");
+    scanf("%f", &height);
+    fflush(stdin);
+    printf("Digite o seu nome: \n");
+    scanf("%s", name);
+    fflush(stdin);
+#endif
     printf(" -- Dados informados: -- \n");
-    printf("idade %d \n", age);
+    printf("Idade %d \n", age);
     printf("Altura: %.2f \n", height);
-    printf("Name: %s", name);
+    printf("Nome: %s", name);
+
+    return 0;
 }
