05ddb17493d0b17a10f1325114c2137a1cdea2c3
An Jorge <an.jorge@icloud.com>
2024-11-19 00:12:19 +0100

    style out

--- Exercicios/02/main.c
+++ Exercicios/02/main.c
@@ -3,14 +3,12 @@
 
 int main()
 {
-    setlocale(LC_ALL, "Portuguese \n");
+    setlocale(LC_ALL, "Portuguese");
     printf("|||| ||| |||||| |||||| ||||| |||||||||| \n\n");
 
     printf(" | CALCULO DE MÉDIA DO ALUNO | \n\n");
-    
-    printf("|||| ||| |||||| |||||| ||||| |||||||||| \n\n");
 
-    
+    printf("|||| ||| |||||| |||||| ||||| |||||||||| \n\n");
 
     int first_exam = 10;
     int second_exam = 5;
@@ -21,7 +19,6 @@ int main()
     printf("(2) %d \n", second_exam);
     printf("(3) %d \n", third_exam);
     sum_exams = first_exam + second_exam + third_exam / 3;
-    printf("%dv é a nota média \n\n",sum_exams);
+    printf("%dv é a nota média \n\n", sum_exams);
     return 0;
-    
 }
