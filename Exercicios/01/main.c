631e560af700a1b7cff233c63a0c7a65023989c7
An Jorge <an.jorge@icloud.com>
2024-11-19 00:32:25 +0100

    style write

--- Exercicios/01/main.c
+++ Exercicios/01/main.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-    int number_first = 10;
+    int first_number = 10;
     int second_number = 5;
     int sum_numbers;
 
-    sum_numbers = number_first + second_number;
-    printf("O resultado da soma entre %d + %d = %d", number_first, second_number, sum_numbers);
+    sum_numbers = first_number + second_number;
+    printf("O resultado da soma entre %d + %d = %d", first_number, second_number, sum_numbers);
 
     return 0;
 }
